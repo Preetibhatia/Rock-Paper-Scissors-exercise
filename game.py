e23d1aee310c07ee5c1b28e0d9bb52cbd02bd326
Preetibhatia <50919836+Preetibhatia@users.noreply.github.com>
2019-06-05 20:20:44 -0400

    Update game.py

--- game.py
+++ game.py
@@ -24,6 +24,30 @@ if user_choice not in ["rock","paper","scissor"]:
 print ("Generating.......")
 computer_choice = random.choice(["rock","paper","scissor"])
 print("Computer choice:", computer_choice)
+
+
 #Determine  The winner
+#rock beats scissors
+#paper beats rock
+#scissors beat paper
+#same selection is a tie
+
+if user_choice == computer_choice:
+    print ("It's a Tie!!")
+
+elif user_choice == "rock" and computer_choice == "paper":
+    print("paper")
+elif user_choice == "rock" and computer_choice =="scissors":
+    print("rock")
+
+elif user_choice=="paper" and computer_choice=="rock":
+    print("paper")  
+elif user_choice=="paper" and computer_choice=="scissors":
+    print("scissors")
+
+elif user_choice=="scissors" and computer_choice=="rock":
+    print("rock")
+elif user_choice=="scissors" and computer_choice=="paper":
+    print("scissors")
 
 #display final outputs
